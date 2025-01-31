d01fb8372b774a9f5a54ddbb5e41889c53732c81
Hugo <hg.targino@gmail.com>
2025-01-31 10:58:56 -0300

    Utilizando test data builder no teste do repositório em memória de categoria

--- src/category/infra/db/__test__/category-in-memory.repository.spec.ts
+++ src/category/infra/db/__test__/category-in-memory.repository.spec.ts
@@ -1,3 +1,4 @@
+import { CategoryFakeBuilder } from "../../../domain/category-fake.builder";
 import { Category } from "../../../domain/category.entity";
 import { CategoryInMemoryRepository } from "../category-in-memory.repository";
 
@@ -6,7 +7,7 @@ describe("CategoryInMemoryRepository", () => {
 
   beforeEach(() => (repository = new CategoryInMemoryRepository()));
   it("should no filter items when filter object is null", async () => {
-    const items = [Category.create({ name: "test" })];
+    const items = [Category.fake().aCategory().build()];
     const filterSpy = jest.spyOn(items, "filter" as any);
 
     const itemsFiltered = await repository["applyFilter"](items, null);
@@ -16,9 +17,9 @@ describe("CategoryInMemoryRepository", () => {
 
   it("should filter items using filter parameter", async () => {
     const items = [
-      new Category({ name: "test" }),
-      new Category({ name: "TEST" }),
-      new Category({ name: "fake" }),
+      Category.fake().aCategory().withName("test").build(),
+      Category.fake().aCategory().withName("TEST").build(),
+      Category.fake().aCategory().withName("fake").build(),
     ];
     const filterSpy = jest.spyOn(items, "filter" as any);
 
@@ -31,15 +32,9 @@ describe("CategoryInMemoryRepository", () => {
     const createdAt = new Date();
 
     const items = [
-      new Category({ name: "test", createdAt }),
-      new Category({
-        name: "TEST",
-        createdAt: new Date(createdAt.getTime() + 100),
-      }),
-      new Category({
-        name: "fake",
-        createdAt: new Date(createdAt.getTime() + 200),
-      }),
+      Category.fake().aCategory().withName("test").withCreatedAt(createdAt).build(),
+      Category.fake().aCategory().withName("TEST").withCreatedAt(new Date(createdAt.getTime() + 100)).build(),
+      Category.fake().aCategory().withName("fake").withCreatedAt(new Date(createdAt.getTime() + 200)).build()
     ];
 
     const itemsSorted = await repository["applySort"](items, null, null);
@@ -48,9 +43,9 @@ describe("CategoryInMemoryRepository", () => {
 
   it("should sort by name", async () => {
     const items = [
-      Category.create({ name: "c" }),
-      Category.create({ name: "b" }),
-      Category.create({ name: "a" }),
+      Category.fake().aCategory().withName('c').build(),
+      Category.fake().aCategory().withName('b').build(),
+      Category.fake().aCategory().withName('a').build(),
     ];
 
     let itemsSorted = await repository["applySort"](items, "name", "asc");
