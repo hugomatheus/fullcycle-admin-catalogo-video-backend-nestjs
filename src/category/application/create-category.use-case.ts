822ff7bac3e420cc2d3a15f2dc76ffc8d1818a68
Hugo <hg.targino@gmail.com>
2025-02-11 09:57:06 -0300

    Caso de uso de criação de Categoria

---
+++ src/category/application/create-category.use-case.ts
@@ -0,0 +1,35 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { Category } from "../domain/category.entity";
+import { ICategoryRepository } from "../domain/category.repository";
+
+export class CreateCategoryUseCase
+  implements IUseCase<CreateCategoryInput, CreateCategoryOutput>
+{
+  constructor(private readonly categoryRepository: ICategoryRepository) {}
+
+  async execute(input: CreateCategoryInput): Promise<CreateCategoryOutput> {
+    const entity = Category.create(input);
+    await this.categoryRepository.insert(entity);
+    return {
+      id: entity.categoryId.id,
+      name: entity.name,
+      description: entity.description,
+      isActive: entity.isActive,
+      createdAt: entity.createdAt,
+    };
+  }
+}
+
+export type CreateCategoryInput = {
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+};
+
+export type CreateCategoryOutput = {
+  id: string;
+  name: string;
+  description?: string | null;
+  isActive?: boolean;
+  createdAt: Date;
+};
