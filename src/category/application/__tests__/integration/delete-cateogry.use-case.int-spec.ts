0810a6b8b028c33da331e7a10b5c9e1458f432d8
Hugo <hg.targino@gmail.com>
2025-03-19 15:48:02 -0300

    Testes de caso de uso de exclusão de categoria

---
+++ src/category/application/__tests__/integration/delete-cateogry.use-case.int-spec.ts
@@ -0,0 +1,36 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.value-object";
+import { setupSequelize } from "../../../../shared/infra/testing/helpers.sequelize";
+import { Category } from "../../../domain/category.entity";
+import { CategoryModel } from "../../../infra/db/sequelize/category.model";
+import { CategorySequelizeRepository } from "../../../infra/db/sequelize/category.sequelize.repository";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Integration tests", () => {
+  let repository: CategorySequelizeRepository;
+  let useCase: DeleteCategoryUseCase;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throw delete a category", async () => {
+    const uuid = new Uuid();
+    await expect(useCase.execute(uuid)).rejects.toThrow(
+      new NotFoundError(uuid, Category)
+    );
+  });
+
+  it("should delete a category", async () => {
+    const spyDelete = jest.spyOn(repository, "delete");
+    const entity = Category.create({ name: "Movie" });
+    await repository.insert(entity);
+
+    await useCase.execute({ id: entity.categoryId.id });
+    expect(spyDelete).toHaveBeenCalledTimes(1);
+    await expect(repository.findById(entity.categoryId)).resolves.toBeNull();
+  });
+});
---
+++ src/category/application/__tests__/unit/delete-cateogry.use-case.spec.ts
@@ -0,0 +1,33 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.value-object";
+import { Category } from "../../../domain/category.entity";
+import { CategoryInMemoryRepository } from "../../../infra/db/in-memory/category-in-memory.repository";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Units tests", () => {
+  let repository: CategoryInMemoryRepository;
+  let useCase: DeleteCategoryUseCase;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throw delete a category", async () => {
+    const uuid = new Uuid();
+    await expect(useCase.execute(uuid)).rejects.toThrow(
+      new NotFoundError(uuid, Category)
+    );
+  });
+
+  it("should delete a category", async () => {
+    const spyDelete = jest.spyOn(repository, "delete");
+    const entity = Category.create({ name: "Movie" });
+    await repository.insert(entity);
+
+    await useCase.execute({ id: entity.categoryId.id });
+    expect(spyDelete).toHaveBeenCalledTimes(1);
+    await expect(repository.findById(entity.categoryId)).resolves.toBeNull();
+    expect(repository.items).toHaveLength(0);
+  });
+});
